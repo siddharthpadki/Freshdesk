89356da2ee16f30672fadaa8a3fde41b0b3a9b8b
siddharthpadki <siddharth.padki@btinternet.com>
2018-06-16 15:11:59 +0100

    Add files via upload

---
+++ Freshdesk/FreshdeskApi.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Nito.AsyncEx;
+using Newtonsoft.Json.Linq;
+
+namespace Freshdesk
+{
+    class FreshdeskApi
+    {
+        private HttpClient httpClient;
+
+        public FreshdeskApi()
+        {
+            httpClient = new HttpClient();
+            System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+            httpClient.BaseAddress = new Uri("https://siddharth.freshdesk.com/api/v2/");
+            httpClient.DefaultRequestHeaders
+              .Accept
+              .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var byteArray = Encoding.ASCII.GetBytes("ysJkz1KFQjlUQkFHfERJ:X");
+            httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+
+            Console.WriteLine("Created FreshdeskApi.");
+        }
+
+        public async Task<string> createCategory(string name, string description)
+        {
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "solutions/categories");
+            var body = "{\"name\":\"" + name + "\",\"description\":\"" + description + "\"}";
+            request.Content = new StringContent(body, Encoding.UTF8, "application/json");
+
+            var response = await httpClient.SendAsync(request);
+            var data = await response.Content.ReadAsStringAsync();
+            var jObject = JObject.Parse(data);
+            var categoryId = jObject["id"].ToString();
+
+            Console.WriteLine(string.Format("Created Category {0}. Name: {1}; Description: {2}", categoryId, name, description));
+            return categoryId;
+        }
+
+        public async Task<string> createFolder(string categoryId, string name, string description, int visibility)
+        {
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, string.Format("solutions/categories/{0}/folders", categoryId));
+            var body = "{\"name\":\"" + name + "\",\"description\":\"" + description + "\",\"visibility\":" + visibility + "}";
+            request.Content = new StringContent(body, Encoding.UTF8, "application/json");
+
+            var response = await httpClient.SendAsync(request);
+            var data = await response.Content.ReadAsStringAsync();
+            var jObject = JObject.Parse(data);
+            var folderId = jObject["id"].ToString();
+
+            Console.WriteLine(string.Format("Created Folder {0}. Name: {1}; Description: {2}; Visibility: {3}", folderId, name, description, visibility));
+            return folderId;
+        }
+        public async Task<string> createArticle(string folderId, string title, string description, int type, int status)
+        {
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, string.Format("solutions/folders/{0}/articles", folderId));
+            var body = "{\"title\":\"" + title + "\",\"description\":\"" + description + "\",\"type\":" + type + ",\"status\":" + status + "}";
+            request.Content = new StringContent(body, Encoding.UTF8, "application/json");
+
+            var response = await httpClient.SendAsync(request);
+            var data = await response.Content.ReadAsStringAsync();
+            var jObject = JObject.Parse(data);
+            var articleId = jObject["id"].ToString();
+
+            Console.WriteLine(string.Format("Created Article {0}. Title: {1}; Description: {2}; Type: {3}; Status: {4}", articleId, title, description, type, status));
+            return articleId;
+        }
+
+        public async Task<List<string>> getCategories()
+        {
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "solutions/categories");
+            var response = await httpClient.SendAsync(request);
+            var data = await response.Content.ReadAsStringAsync();
+            var jToken = JToken.Parse(data);
+            Console.WriteLine("Got Categories: " + jToken.ToString());
+            var categoryIds = new List<string>();
+            foreach (JObject categoryJObject in jToken.Children())
+            {
+                var categoryId = categoryJObject["id"].ToString();
+                categoryIds.Add(categoryId);
+            }
+            return categoryIds;
+        }
+
+        public async Task deleteCategory(string categoryId)
+        {
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Delete, string.Format("solutions/categories/{0}", categoryId));
+            Console.WriteLine("Deleted Category " + categoryId);
+            await httpClient.SendAsync(request);
+        }
+
+        public async Task deleteAllCategories()
+        {
+            var categoryIds = await getCategories();
+            var deleteTasks = new List<Task>();
+            foreach (string categoryId in categoryIds)
+            {
+                deleteTasks.Add(deleteCategory(categoryId));
+            }
+            foreach (Task deleteTask in deleteTasks)
+            {
+                await deleteTask;
+            }
+        }
+    }
+}
---
+++ Freshdesk/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Nito.AsyncEx;
+
+namespace Freshdesk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AsyncContext.Run(() => MainAsync(args));
+        }
+
+        static async void MainAsync(string[] args)
+        {
+            FreshdeskApi api = new FreshdeskApi();
+
+            await api.deleteAllCategories();
+
+            string analyticsCategoryId = await api.createCategory("Analytics", "Analytics category.");
+            string monitoringCategoryId = await api.createCategory("Monitoring", "Monitoring category.");
+            string operationsCategoryId = await api.createCategory("Operations", "Operations category.");
+
+            string configurationFolderId = await api.createFolder(analyticsCategoryId, "Configuration", "Configuration folder.", 1);
+            string gettingStartedFolderId = await api.createFolder(analyticsCategoryId, "Getting Started", "Getting started in Analytics Folder", 1);
+
+            string articleId = await api.createArticle(configurationFolderId, "how-to-configure-analytics", "How to configure analytics.", 1, 1);
+            string analyticsArticleId = await api.createArticle(gettingStartedFolderId, "getting-started-with-analytics", "Getting started with Analytics", 1, 1);
+
+            string monitoringConfigurationFolderId = await api.createFolder(monitoringCategoryId, "Configuration", "Configuration folder in monitoring", 1);
+            string configurationHelpArticleId = await api.createArticle(monitoringConfigurationFolderId, "configuration-help", "Configuration Help", 1, 1);
+
+            string monitoringGettingStartedFolderId = await api.createFolder(monitoringCategoryId, "Getting Started", "Getting started in monitoring folder", 1);
+            string monitoringStartedArticleId = await api.createArticle(monitoringGettingStartedFolderId, "getting-started-with-monitoring", "Getting started with Monitoring", 1, 1);
+
+            string operationsFolderId = await api.createFolder(operationsCategoryId, "getting-started-with-biztalk360", "Getting started with biztalk360", 1);
+            string operationsArticleId = await api.createArticle(operationsFolderId, "getting-started-with-biztalk360", "This article explains how to get started with biztalk360", 1, 1);
+
+            string documentationCategoryId = await api.createCategory("Documentation", "Documentation category");
+            string documentationFolderId = await api.createFolder(documentationCategoryId, "getting-started-with-biztalk360", "Getting started with biztalk360", 1);
+            string documentationArticleId = await api.createArticle(documentationFolderId, "getting-started-with-biztalk360", "This article explains how to get started with biztalk360. This is new addition to the existing article.", 1, 1);
+
+            string newBarnCategoryId = await api.createCategory("New Barn Category", "This is a test category");
+            Console.ReadKey();
+        }
+
+    }
+}
